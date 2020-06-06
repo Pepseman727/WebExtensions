c8601b1ce3c438b6ca03f8f547acb57934a71d84
Kirill <pepseman727@gmail.com>
2020-06-07 01:25:01 +0300

    full work version

--- Site Block/Extension/block.js
+++ Site Block/Extension/block.js
@@ -1,40 +1,32 @@
-function BlockPage(request, sender, sendResponse) {
-    document.body.textContent = "";
-
-    let warning = document.createElement('h1');
-
-    warning.textContent = "Sorry, but you have no access to this WebSite :(";
-    document.body.appendChild(warning);
-
+//На всякий случай
+/* function GetURL() {
     let regex = /(.*:)\/{2,}([\w\-\._]+)?/igu
 
     let blockedURL = location.href.match(regex);
     console.log(blockedURL);
-
-   /* let manifest = browser.runtime.getManifest();
-    console.log(manifest);*/
-
-    getFile('manifest.json');
-
+    return blockedURL;
 }
+
+function AddURL() {
+    let url = GetURL();
+    localStorage.setItem('site', url);
+
+}*/
+
+function BlockPage(request, sender, sendResponse) {
+    document.body.textContent = "";
+    document.body.style.cssText = 'background-color: #f11b1b; text-align: center;'
+
+    let warning = document.createElement('h1');
+    warning.style.cssText = 'color: white;  font-family: "Calibri";  padding: 3em 0;  font-size: 20px;'
+
+    warning.textContent = "Sorry, but you have no access to this WebSite :(";
+    document.body.appendChild(warning);
+
+    localStorage.setItem('blocked', 'YES');
+}
+
+
+
 
 browser.runtime.onMessage.addListener(BlockPage);
-
-var DATA;
-
-function getFile(fileName) {
-
-    var request = new XMLHttpRequest();
-    request.open('GET', fileName);
-    request.onloadend = function () {
-        parse(request.responseText);
-    }
-    request.send();
-}
-
-function parse(obj) {
-
-    DATA = JSON.parse(obj);
-
-    console.log(DATA);
-}
--- Site Block/Extension/instant_block.js
+++ Site Block/Extension/instant_block.js
@@ -1,6 +1,20 @@
-document.body.textContent = "";
+function BlockSite() {
+    document.body.textContent = "";
+    document.body.style.cssText = 'background-color: #f11b1b; text-align: center;'
 
-let warning = document.createElement('h1');
+    let warning = document.createElement('h1');
+    warning.style.cssText = 'color: white;  font-family: "Calibri";  padding: 3em 0;  font-size: 20px;'
 
-warning.textContent = "Sorry, but you have no access to this WebSite :(";
-document.body.appendChild(warning);
+    warning.textContent = "Sorry, but you have no access to this WebSite :(";
+    document.body.appendChild(warning);
+
+}
+
+let blocked = localStorage.getItem('blocked');
+if (blocked === 'YES') {
+    BlockSite();
+}
+else {
+    console.log('HUY');
+}
+//«апускаетс€ скрипт, провер€ю Ч есть ли URL в списке запрещЄнных и потом блокирую
